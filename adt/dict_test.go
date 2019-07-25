7911ec205a21029b59cf1b39a007b3d4edc0b9af
Dragonbuf <sylarzhang@outlook.com>
2019-07-25 16:40:46 +0800

    dict: 修复 dict 跟跟进　*key 指针判断　key 是否相等 bug

--- adt/dict_test.go
+++ adt/dict_test.go
@@ -6,8 +6,8 @@ import (
 
 func TestDict(t *testing.T) {
 	dict := NewDict()
-	k := "a"
-	v := "b"
+	k := "fuck"
+	v := "you"
 	k2 := "c"
 	v2 := "d"
 	k3 := "e"
@@ -15,10 +15,16 @@ func TestDict(t *testing.T) {
 	k4 := "g"
 
 	dict.HsetString(&k, &v)
+
+	res := dict.HgetString(&k2)
+	if res == v {
+		t.Error("hget k2 is wrong ", res)
+	}
+
 	dict.HsetString(&k2, &v2)
 	dict.HsetString(&k3, &v3)
 
-	res := dict.HgetString(&k)
+	res = dict.HgetString(&k)
 	if res != v {
 		t.Error("hget k is wrong ", res)
 	}
